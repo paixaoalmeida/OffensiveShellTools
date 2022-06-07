ec039841d73fba453f9c1e1ec935e02e632145fb
White Rose <85945510+paixaoalmeida@users.noreply.github.com>
2022-06-06 23:27:38 -0300

    Update BruteForceDomain.sh
    DNS RECON USING DNS REGISTERS
    
    IT WORKS REALLY WELL!

--- BruteForceDomain.sh
+++ BruteForceDomain.sh
@@ -9,6 +9,11 @@
 #nova estrutura de código com elif etc
 #É necessário ter uma lista para o brute force (listadns.txt) (eu tenho no meu sistema)
 
+#Alterações para fazer um reonhecimento de mais subdominios e seus ALIAS, também serve
+#para tentar fazer subdomain takeover #dia 6 de junho
+#Faz uma pesquisa pelos registros CNAMES dos subdominios para tentar achar subdominios interessantes
+#função -s #dia 6 de junho
+
 domain=$2
 
 COR_VERMELHO="\e[31;1;4m \n"
@@ -18,15 +23,20 @@ COR_YELLOW="\e[33;1m"
 MENSAGEM_DE_AJUDA="
 $(basename $0) - BruteForcDomain / MENU DE AJUDA
 MODO DE USO:
-$0 -a URL sem o WWW
+$0 -a URL sem o WWW (Faz brute force de subdominios)
+$0 -s URL sem o WWW (Faz brute force de subdominios mas usando os registros CNAMES)
+
+By WhiteRose / github.com/paixaoalmeida
 "
 #-------------------------------------------------------------------------------
-if [ $1 =  ];then
+if [ $1 =  ];then          #if for the script
   echo $MENSAGEM_DE_AJUDA
 elif [ $1 = "-a" ];then
   while read sub;do
-    if host $sub.$domain &> /dev/null;then
+    if host $sub.$domain &> /dev/null;then          #comand host and default output
       echo -e ${COR_VERMELHO} "Dominios encontrados:" ${COR_YELLOW}"$sub.$domain";
     fi
-  done < listadns.txt
+  done < listadns.txt          #u need to have that "listadns.txt" in ur path, its in the repository
+elif [ $1 = "-s" ];then
+  for dns in $(cat listadns.txt);do host -t cname $dns.$domain;done | grep "alias for" #consulting valid cname registers
 fi
